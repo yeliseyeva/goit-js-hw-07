6bbaeb3baa343f9761f2c4dac838eb078f0e4f1f
yeliseyeva <74910968+yeliseyeva@users.noreply.github.com>
2021-07-05 11:36:37 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,11 +7,20 @@ const ingredients = [
   'Приправы',
 ];
 
-for (const ingredient of ingredients) {
+// for (const ingredient of ingredients) {
+//   const itemEl = document.createElement('li');
+//   itemEl.textContent = ingredient;
+//   console.log(itemEl)
+
+//   const list = document.querySelector('#ingredients');
+//   list.append(itemEl)
+// }
+
+const list = document.querySelector('#ingredients');
+
+ingredients.map((ingredient) => {
   const itemEl = document.createElement('li');
   itemEl.textContent = ingredient;
-  console.log(itemEl)
 
-  const list = document.querySelector('#ingredients');
   list.append(itemEl)
-}
+})
